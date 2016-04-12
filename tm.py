c0d001175b0e37708d69bad3491cef3aa6b0f585
Etienne Kintzler <etienne.kintzler@gmail.com>
2016-04-12 09:05:34 +0200

    Update tm.py

--- tm.py
+++ tm.py
@@ -1,11 +1,11 @@
 docs = []
 from os import listdir, chdir, getcwd
-import re
+import re, codecs, stop_words
+path_input = "/home/kintzler/R/projet_boucheron/maildir/"
+path_output ="/home/kintzler/R/projet_boucheron/DATA/"
 
-import codecs
-import stop_words 
 stopwords = stop_words.get_stop_words('en')
-stopwords.extend("asap http www".split())
+stopwords.extend("asap http www fyi".split())
 stopwords.extend(open("stopwords.txt","r").read().split())
 
 stopwords = list(set(stopwords)) #supprime les doublons
@@ -25,7 +25,7 @@ date_pat = re.compile("Date:.+\n")
 xmail_pat = re.compile("X-Mailer:.+\n")
 mimver_pat = re.compile("MIME-Version:.+\n")
 
-fwd_pat = re.compile("Forwarded")
+fwd_pat = re.compile("Forwarded:.+\n")
 
 # The enron emails are in 151 directories representing each each senior management
 # employee whose email account was entered into the dataset.
@@ -34,7 +34,6 @@ fwd_pat = re.compile("Forwarded")
 # I've used readlines() to read in the emails because read() 
 # didn't seem to work with these email files.
 
-path_input = "/home/kintzler/R/projet_boucheron/DATA2/"
 chdir(path_input)
 names = [d for d in listdir(".") if "." not in d] #listdir(".") noms de tous les folders
 for name in names:
@@ -77,26 +76,26 @@ for subfolder in docs:
         email_new = xmail_pat.sub('', email_new)
         email_new = mimver_pat.sub('', email_new)
         email_new = fwd_pat.sub('', email_new)
-        
         email_new = email_new.lower()
         email_new = re.sub("[^a-zA-Z'-]", " ", email_new) #on conserve le " ' " avant de retirer le stop words
-        email_new  = ' '.join([word for word in email_new.split() if word.lower() not in stopwords])
-        email_new  = ' '.join([word for word in email_new.split() if len(word)>2])
-        email_new = re.sub("[^a-zA-Z-]", " ", email_new) #on peut retirer les " ' " après avoir retirer les stop words
+        email_new = ' '.join([word for word in email_new.split() if word.lower() not in stopwords])
+        email_new = ' '.join([word for word in email_new.split() if len(word)>2])
+        email_new = re.sub("[^a-zA-Z]", " ", email_new) #on peut retirer les " ' " après avoir retirer les stop words
+        email_new = re.sub(' +',' ',email_new)
         docs_final.append(email_new)
 
 # Here I proceed to dump each and every email into about 126 thousand separate 
 # txt files in a newly created 'data' directory.  This gets it ready for entry into a Corpus using the tm (textmining)
 # package from R.
 
-path_output ="/home/kintzler/R/projet_boucheron/sub2/"
+
 
 for n, doc in enumerate(docs_final):
     outfile = open(path_output+"%s.txt" % n ,'w')
     outfile.write(doc)
     outfile.write('\n')
     outfile.close()
-    if n>= 10000:
-        break
+    #if n>= 10000:
+    #    break
         
 print("Fin du script atteint!")

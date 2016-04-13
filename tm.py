5409819aca85480cca19a42511950925a6316f4c
Etienne Kintzler <etienne.kintzler@gmail.com>
2016-04-13 10:57:03 +0200

    Update tm.py

--- tm.py
+++ tm.py
@@ -1,14 +1,14 @@
 docs = []
-from os import listdir, chdir
-import re
+from os import listdir, chdir, getcwd
+import re, codecs, stop_words
+import numpy as np
+path_input = "/home/kintzler/R/projet_boucheron/maildir/"
+path_output = "/home/kintzler/R/projet_boucheron/DATA/"
 
-import codecs
-import stop_words 
 stopwords = stop_words.get_stop_words('en')
-other= "like want thank thanks hi http www com much must just"
-stopwords.extend(other.split())
-stopwords.extend(["a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also","although","always","am","among", "amongst", "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as",  "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"])
-
+stopwords.extend("asap http www fyi hotmail email e-mail sent forwarded msn asked find send sent ect mail enron".split())
+stopwords.extend(open("stopwords.txt","r").read().split())
+stopwords = sorted(list(set(stopwords))) #supprime les doublons
 
 # Here's my attempt at coming up with regular expressions to filter out
 # parts of the enron emails that I deem as useless.
@@ -24,35 +24,21 @@ reply_pat = re.compile("Reply- Organization:.+\n")
 date_pat = re.compile("Date:.+\n")
 xmail_pat = re.compile("X-Mailer:.+\n")
 mimver_pat = re.compile("MIME-Version:.+\n")
+fwd_pat = re.compile("Forwarded:.+\n")
 
-fwd_pat = re.compile("Forwarded")
 
-# The enron emails are in 151 directories representing each each senior management
-# employee whose email account was entered into the dataset.
-# The task here is to go into each folder, and enter each 
-# email text file into one long nested list.
-# I've used readlines() to read in the emails because read() 
-# didn't seem to work with these email files.
-
-path_input = "/home/kintzler/R/projet_boucheron/DATA2/"
 chdir(path_input)
-names = [d for d in listdir(".") if "." not in d] #listdir(".") noms de tous les folders
+names = [d for d in listdir(".") if "." not in d]
 for name in names:
     print(name)
     chdir(path_input+"%s" % name) #"%s"=string
     subfolders = listdir('.')
-    sent_dirs = [n for n, sf in enumerate(subfolders) if "sent" in sf] #enumerate, retourne pour chaque i, l'itération i suivi du nom
+    sent_dirs = [n for n, sf in enumerate(subfolders) if "sent" in sf] 
     sent_dirs_words = [subfolders[i] for i in sent_dirs]
     for d in sent_dirs_words:
         chdir(path_input+'%s/%s' % (name,d))
         file_list = listdir('.')
-        docs.append([" ".join(codecs.open(f, 'r','latin-1').readlines()) for f in file_list if "." in f]) 
-        #docs.append : ajout à la list append
-        #open(f,'r') r signifie qu'on est en mode lecture "read"; s="-", seq=("a","b","c"), s.join(seq) --> 'a-b-c'
-        
-# Here i go into each email from each employee, try to filter out all the useless stuff,
-# then paste the email into one long flat list.  This is probably inefficient, but oh well - python
-# is pretty fast anyway!
+        docs.append([" ".join(codecs.open(f, 'r','latin-1').readlines()) for f in file_list if "." in f
 
 docs_final = []
 for subfolder in docs:
@@ -77,26 +63,23 @@ for subfolder in docs:
         email_new = xmail_pat.sub('', email_new)
         email_new = mimver_pat.sub('', email_new)
         email_new = fwd_pat.sub('', email_new)
-        
-        email_new = email_new.lower()
-        email_new = re.sub("[^a-zA-Z'-]", " ", email_new) #on conserve le " ' " avant de retirer le stop words
-        email_new  = ' '.join([word for word in email_new.split() if word.lower() not in stopwords])
-        email_new  = ' '.join([word for word in email_new.split() if len(word)>2])
-        email_new = re.sub("[^a-zA-Z-]", " ", email_new) #on peut retirer les " ' " après avoir retirer les stop words
+        email_new = re.sub("[^a-zA-Z]", " ", email_new)
+        email_new = ' '.join([word.lower() for word in email_new.split() if word.lower() not in stopwords])
+        email_new = ' '.join([word for word in email_new.split() if len(word)>2 and len(word)<30])
+        email_new = re.sub(' +',' ',email_new)
         docs_final.append(email_new)
 
 # Here I proceed to dump each and every email into about 126 thousand separate 
 # txt files in a newly created 'data' directory.  This gets it ready for entry into a Corpus using the tm (textmining)
 # package from R.
 
-path_output ="/home/kintzler/R/projet_boucheron/sub2/"
+
 
 for n, doc in enumerate(docs_final):
-    outfile = open(path_output+"%s.txt" % n ,'w')
-    outfile.write(doc)
-    outfile.write('\n')
-    outfile.close()
-    if n>= 10000:
-        break
+    if np.random.rand()<0.3:
+        outfile = open(path_output+"%s.txt" % n ,'w')
+        outfile.write(doc)
+        outfile.write('\n')
+        outfile.close
         
-print("Fin du script atteint!")
+print("Fin du script")
